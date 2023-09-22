0a4ec09ed20bf28ffdf5bf03e49190166d5f05f6
Zahradeen Muazu <zahradeenmuazu@yahoo.com>
2023-09-22 14:12:48 +0100

    added data validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -13,6 +13,7 @@ import { createTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 import { GetTasksFilterDto } from './dto/get-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -47,8 +48,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('staus') status: TaskStatus,
+    @Body() updateTaskStatus: UpdateTaskStatusDto,
   ): Task {
+    const { status } = updateTaskStatus;
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { createTaskDto } from './dto/create-task.dto';
@@ -13,14 +13,15 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    console.log(
-      this.tasks.find((task) => {
-        return task.id === id;
-      }),
-    );
-    return this.tasks.find((task) => {
+    const found = this.tasks.find((task) => {
       return task.id === id;
     });
+
+    if (!found) {
+      throw new NotFoundException(`Task with ${id} not found`);
+    }
+
+    return found;
   }
 
   getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
@@ -61,7 +62,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
