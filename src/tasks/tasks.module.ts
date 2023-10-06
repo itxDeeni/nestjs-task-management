83c5e81c98a9f34d9dd0e86d268bdb2084521213
Zahradeen Muazu <zahradeenmuazu@yahoo.com>
2023-10-06 12:41:47 +0100

    added signin and jwt debug

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,11 +4,12 @@ import { TasksController } from './tasks.controller';
 import { Task } from './tasks.entity';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TasksRepository],
-  exports:[TasksRepository]
+  exports: [TasksRepository],
 })
 export class TasksModule {}
